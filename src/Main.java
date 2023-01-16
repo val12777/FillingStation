5728fddef56bef809f84bb22876c1d516bac6334
Valen <valentinhamidullin78@gmail.com>
2023-01-16 14:03:55 +0300

    FillingStation изм.1

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,41 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Система расчета стоимости топлива");
+
         int fuelType = 95;
-        int amount = 50;
+        int amount = 420;
+
         double fuel92price = 60.2;
         double fuel95price = 67.33;
+        int maxAmount = 400;
+
         double fuelPrice = 0;
         if (fuelType == 92) {
             fuelPrice = fuel92price;
-        }
-        if (fuelType == 95) {
+        } else if (fuelType == 95) {
             fuelPrice = fuel95price;
+        } else {
+            System.out.println("Указан неверный тип топлива");
         }
+
+        if (amount < 1) {
+            System.out.println("Указано слишком малое количество топлива");
+            amount = 0;
+        }
+
+        if (amount > maxAmount) {
+            System.out.println("Указанное количество топлива превышает максимально допустимое");
+                    amount = maxAmount;
+        }
+
+
         System.out.println("Цена выбранного топлива: " + fuelPrice + " руб.");
+
         double totalPrice = fuelPrice * amount;
         System.out.println("Общая стоимость заправки: " + totalPrice + " руб.");
         if (fuelType > 0) {
-        } else {
-            System.out.println("Выбран неверный тип топлива");
-        }
-        if (amount < 1) {
-        } else {
-            System.out.println("Указано слишком малое количество топлива");
-            amount = 0;
+
+
         }
     }
 }
